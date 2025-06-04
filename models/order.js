29705aae45b8e337021bf132d24fba7cbf605136
Pluton Hades <88476549+nobodie0raj@users.noreply.github.com>
2025-06-04 13:32:11 +0530

    Update and rename Order.js to order.js

---
+++ models/order.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  service: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  link: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
